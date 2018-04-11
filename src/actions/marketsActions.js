ef6df5e66f95701992fca4668cadaa4e0cb7fda4
ankit <ankit.sari@gmail.com>
2018-04-11 19:24:08 +0530

    initial commit

---
+++ src/actions/marketsActions.js
@@ -0,0 +1,51 @@
+import { SET_COMPONENTS } from './types';
+import axios from 'axios';
+
+export function setComponents(payload) {
+  return {
+    type: SET_COMPONENTS,
+    payload
+  };
+}
+
+export function fetchMarkets() {
+  return dispatch => {
+    return axios.get(process.env.API_URL + '/api/markets').then(res => {
+      if (res.status !== 200) {
+        console.log(`There was a problem: ${res.status}`);
+        return;
+      }
+      //dispatch(setMarkets(res.data.markets));
+    }, err => {
+
+    });
+  }
+}
+
+export function fetchCompanies() {
+  return dispatch => {
+    return axios.get(process.env.REACT_APP_API_URL).then(res => {
+      if (res.status !== 200) {
+        console.log(`There was a problem: ${res.status}`);
+        return;
+      }
+      //dispatch(setComponents(res.data));
+    }, err => {
+
+    });
+  }
+}
+
+export function fetchComponents() {
+  return dispatch => {
+    return axios.get(process.env.REACT_APP_API_URL).then(res => {
+      if (res.status !== 200) {
+        console.log(`There was a problem: ${res.status}`);
+        return;
+      }
+      dispatch(setComponents(res.data));
+    }, err => {
+
+    });
+  }
+}
